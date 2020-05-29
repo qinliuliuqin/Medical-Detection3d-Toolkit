d77f6134c976ef5a83981ad44bbeaca4b89a1009
Qin Liu <sjtuliuqin@163.com>
2020-05-29 16:38:19 -0400

    Update

--- detection3d/lmk_det_train.py
+++ detection3d/lmk_det_train.py
@@ -5,17 +5,18 @@ from detection3d.core.lmk_det_train import train
 
 def main():
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '2'
-    os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-
     long_description = "Training engine for 3d medical image landmark detection"
     parser = argparse.ArgumentParser(description=long_description)
 
     parser.add_argument('-i', '--input',
                         default='/home/ql/projects/Medical-Detection3d-Toolkit/detection3d/config/lmk_train_config.py',
                         help='configure file for medical image segmentation training.')
+    parser.add_argument('-g', '--gpus',
+                        default='0',
+                        help='the device id of gpus.')
     args = parser.parse_args()
 
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
     train(args.input)
 
 
